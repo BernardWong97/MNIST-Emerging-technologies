8eb23ae6a5f8732932ada2ed9a92077a0a7f3c02
Bernard Wong <ben.wc88@gmail.com>
2019-11-21 06:25:46 +0000

    Try prediction, wrong output, issue with reshape

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ from io import BytesIO
 import numpy as np
 import base64
 
+import sys
+import os
+
+sys.path.append(os.path.abspath("./model"))
+from load import *
 
 # Instantiate web application
 app = Flask(__name__)
-
+model, graph = init()
 
 # Home page route
 @app.route('/')
@@ -17,9 +22,20 @@ def home():
 
 
 @app.route('/predict', methods=['GET', 'POST'])
-def receive():
+def predict():
     imgbase64 = request.values.get("imgUrl", "0")
-    print(imgbase64)
-    img = Image.open(BytesIO(base64.b64decode(imgbase64)))
+    img = Image.open(BytesIO(base64.b64decode(imgbase64.split(',')[1])))
+    img.save("output.png")
 
-    return "Result"
+    x = Image.open('output.png')
+    x = x.resize((28, 28))
+    x = np.array(x)
+    print(x.shape)
+    x = x.reshape(4, 784)
+
+    with graph.as_default():
+        out = model.predict(x)
+        print(out)
+        print(np.argmax(out, axis=1))
+        response = np.array_str(np.argmax(out, axis=1))
+        return response
